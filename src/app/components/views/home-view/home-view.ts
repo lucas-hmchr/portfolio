fb6888fe2d2e6b03931e95206503042e6eb05084
lucas <lucashamacher01@gmail.com>
2025-09-02 15:44:22 +0200

    add contact section

--- src/app/components/views/home-view/home-view.ts
+++ src/app/components/views/home-view/home-view.ts
@@ -6,11 +6,12 @@ import { WhyMeSection } from '../../content/why-me-section/why-me-section';
 import { SkillSection } from '../../content/skill-section/skill-section';
 import { MyProjects } from '../../content/my-projects/my-projects';
 import { FeedbackSection } from '../../content/feedback-section/feedback-section';
+import { ContactForm } from '../../content/contact-form/contact-form';
 
 
 @Component({
   selector: 'app-home-view',
-  imports: [HeroSection, MobileNav, WhyMeSection, SkillSection, MyProjects, FeedbackSection],
+  imports: [HeroSection, MobileNav, WhyMeSection, SkillSection, MyProjects, FeedbackSection, ContactForm],
   templateUrl: './home-view.html',
   styleUrl: './home-view.scss'
 })
