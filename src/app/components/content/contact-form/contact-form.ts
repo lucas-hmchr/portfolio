47150f3361917cb56a0ca3634341de3b9eacbe76
lucas <lucashamacher01@gmail.com>
2025-09-03 11:59:30 +0200

    adjust contact section

--- src/app/components/content/contact-form/contact-form.ts
+++ src/app/components/content/contact-form/contact-form.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BreakpointService } from '../../../services/breakpoint';
 
 @Component({
   selector: 'app-contact-form',
@@ -8,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class ContactForm {
 
+  constructor(public bp: BreakpointService){
+    
+  }
 }
