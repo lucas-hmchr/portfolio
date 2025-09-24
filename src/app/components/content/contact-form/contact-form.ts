d8e14234aa9dbf2c385de79f02c252c9d0e1b77b
lucas <lucashamacher01@gmail.com>
2025-09-24 12:37:34 +0200

    add simple form validation

--- src/app/components/content/contact-form/contact-form.ts
+++ src/app/components/content/contact-form/contact-form.ts
@@ -1,17 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BreakpointService } from '../../../services/breakpoint';
 import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [TranslocoModule],
+  imports: [TranslocoModule, FormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss'
 })
 export class ContactForm {
 
-  constructor(public bp: BreakpointService){
-    
+  constructor(public bp: BreakpointService) {
+
+  }
+
+  http = inject(HttpClient)
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
   }
 }
