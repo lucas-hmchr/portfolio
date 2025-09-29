fa12c660f308438b2ed5eccd0b95faa2a92dccb2
lucas <lucashamacher01@gmail.com>
2025-09-29 08:33:29 +0200

    add mail to php

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'DEINE@MAIL.de';  
+            $recipient = 'lucashamacher01@gmail.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
