41362b67ba69901368f77cf68d49920c7f3f0500
lucas <lucashamacher01@gmail.com>
2025-09-30 08:22:33 +0200

    responsivness and styling adjustments

--- src/app/components/content/my-projects/my-projects.ts
+++ src/app/components/content/my-projects/my-projects.ts
@@ -23,21 +23,8 @@ export class MyProjects {
         'HTML',
         'CSS',
       ],
-      descriptions: [
-        {
-          title: 'About the project',
-          text: 'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization'
-        },
-        {
-          title: 'How i have organized the process',
-          text: 'How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.'
-        },
-        {
-          title: 'My group work experience',
-          text: 'How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality.What technologies did you use? It is nice to mention good teamwork and cooperation.'
-        },
-      ],
-      img: 'el_pollo_loco.png',
+      descriptions: [ ],
+      img: 'join.png',
       liveLink: 'https://github.com/lucas-hmchr',
       githubLink: 'https://github.com/lucas-hmchr',
       id: 1,
@@ -50,74 +37,35 @@ export class MyProjects {
         'HTML',
         'CSS',
       ],
-      descriptions: [
-        {
-          title: 'About the project',
-          text: 'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization'
-        },
-        {
-          title: 'How i have organized the process',
-          text: 'How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.'
-        },
-        {
-          title: 'My group work experience',
-          text: 'How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality.What technologies did you use? It is nice to mention good teamwork and cooperation.'
-        },
-      ],
+      descriptions: [ ],
       img: 'el_pollo_loco.png',
       liveLink: 'https://github.com/lucas-hmchr',
       githubLink: 'https://github.com/lucas-hmchr',
       id: 2,
     },
     {
-      name: 'Lorem',
+      name: 'Hostel',
       time: '3 Wochen',
       technologies: [
-        'JavaScript',
+        'Wordpress',
         'HTML',
         'CSS',
       ],
-      descriptions: [
-        {
-          title: 'About the project',
-          text: 'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization'
-        },
-        {
-          title: 'How i have organized the process',
-          text: 'How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.'
-        },
-        {
-          title: 'My group work experience',
-          text: 'How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality.What technologies did you use? It is nice to mention good teamwork and cooperation.'
-        },
-      ],
-      img: 'el_pollo_loco.png',
+      descriptions: [ ],
+      img: 'hostel.jpg',
       liveLink: 'https://github.com/lucas-hmchr',
       githubLink: 'https://github.com/lucas-hmchr',
       id: 3,
     },
     {
-      name: 'Ongoing Project',
+      name: 'Ongoing',
       time: '3 Wochen',
       technologies: [
         'JavaScript',
         'HTML',
         'CSS',
       ],
-      descriptions: [
-        {
-          title: 'About the project',
-          text: 'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization'
-        },
-        {
-          title: 'How i have organized the process',
-          text: 'How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.'
-        },
-        {
-          title: 'My group work experience',
-          text: 'How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality.What technologies did you use? It is nice to mention good teamwork and cooperation.'
-        },
-      ],
+      descriptions: [ ],
       img: 'el_pollo_loco.png',
       liveLink: 'https://github.com/lucas-hmchr',
       githubLink: 'https://github.com/lucas-hmchr',
