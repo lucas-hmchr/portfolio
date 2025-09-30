d987d3f09e2bbf5fa183af9ab98b900cb4202851
lucas <lucashamacher01@gmail.com>
2025-09-30 11:01:25 +0200

    expand legal notice and translations

--- src/app/components/views/legal-notice/legal-notice-content/legal-notice-content.ts
+++ src/app/components/views/legal-notice/legal-notice-content/legal-notice-content.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-legal-notice-content',
-  imports: [],
+  imports: [TranslocoModule],
   templateUrl: './legal-notice-content.html',
   styleUrl: './legal-notice-content.scss'
 })
