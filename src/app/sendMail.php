5fe44b83c28604139d98ed235e09b7debfaf0030
lucas <lucashamacher01@gmail.com>
2025-10-06 15:56:13 +0200

    minor fixes and mail script adjustments

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -1,37 +1,37 @@
 <?php
+$allowed_origin = "https://www.portfolio.lucashamacher.de";
 
 switch ($_SERVER['REQUEST_METHOD']) {
-    case ("OPTIONS"): //Allow preflighting to take place.
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: POST");
-        header("Access-Control-Allow-Headers: content-type");
+    case "OPTIONS":
+        header("Access-Control-Allow-Origin: $allowed_origin");
+        header("Access-Control-Allow-Methods: POST, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type");
+        exit(0);
+
+    case "POST":
+        header("Access-Control-Allow-Origin: $allowed_origin");
+
+        $json = file_get_contents('php://input');
+        $params = json_decode($json);
+
+        $email = $params->email;
+        $name = $params->name;
+        $message = $params->message;
+
+        $recipient = 'postmaster@lucashamacher.de';
+        $subject = "Contact From <$email>";
+        $message = "From: " . $name . "<br>" . $message;
+
+        $headers   = [];
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=utf-8';
+        $headers[] = "From: noreply@lucashamacher.de";
+
+        mail($recipient, $subject, $message, implode("\r\n", $headers));
+        echo json_encode(["status" => "success"]);
+        break;
+
+    default:
+        header("Allow: POST", true, 405);
         exit;
-        case("POST"): //Send the email;
-            header("Access-Control-Allow-Origin: *");
-            // Payload is not send to $_POST Variable,
-            // is send to php:input as a text
-            $json = file_get_contents('php://input');
-            //parse the Payload from text format to Object
-            $params = json_decode($json);
-    
-            $email = $params->email;
-            $name = $params->name;
-            $message = $params->message;
-    
-            $recipient = 'lucashamacher01@gmail.com';  
-            $subject = "Contact From <$email>";
-            $message = "From:" . $name . "<br>" . $message ;
-    
-            $headers   = array();
-            $headers[] = 'MIME-Version: 1.0';
-            $headers[] = 'Content-type: text/html; charset=utf-8';
-
-            // Additional headers
-            $headers[] = "From: noreply@mywebsite.com";
-
-            mail($recipient, $subject, $message, implode("\r\n", $headers));
-            break;
-        default: //Reject any non POST or OPTIONS requests.
-            header("Allow: POST", true, 405);
-            exit;
-    } 
+}
